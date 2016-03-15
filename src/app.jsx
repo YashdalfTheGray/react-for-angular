ca8a899ca792979f9fa0f385465eaeb9165a11f8
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-15 19:27:23 -0400

    pulled in the forum page into the app

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import AppBar from 'material-ui/lib/app-bar';
 
+import ForumPage from './components/forum-page';
+
 class App extends React.Component {
-    navMenuClick() {
-        console.log('Nav menu button clicked!');
-    }
 
     render() {
         return (
-            <AppBar
-                title="React Starter"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-                onLeftIconButtonTouchTap={this.navMenuClick} />
+            <div>
+                <AppBar
+                    title="Forum" />
+                <ForumPage />
+            </div>
         );
     }
 }
