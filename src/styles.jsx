dcdbe6c168e991da21bdfdb74f10e494e7034564
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-17 19:47:31 -0400

    added spacer styles to the app

--- src/styles.jsx
+++ src/styles.jsx
@@ -1,5 +1,17 @@
 export default {
     robotoFont: {
         fontFamily: 'Roboto, sans-serif'
+    },
+    spacerSm: {
+        margin: '12px'
+    },
+    spacerMd: {
+        margin: '24px'
+    },
+    spacerLg: {
+        margin: '48px'
+    },
+    spacerHg: {
+        margin: '96px'
     }
 };
