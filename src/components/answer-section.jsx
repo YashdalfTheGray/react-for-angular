e7d260377c39bf0c294cc47292f7ae851c889791
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-17 22:33:22 -0400

    added an answers section with a card and stuff

---
+++ src/components/answer-section.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import * as _ from 'lodash';
+import Card from 'material-ui/lib/card/card';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardText from 'material-ui/lib/card/card-text';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Divider from 'material-ui/lib/divider';
+import CheckIcon from 'material-ui/lib/svg-icons/navigation/check';
+import * as Colors from 'material-ui/lib/styles/colors';
+
+import styles from '../styles';
+
+export default class AnswerSection extends React.Component {
+
+    static propTypes = {
+        answers: React.PropTypes.array.isRequired
+    }
+
+    handleAnswerTap(answer) {
+        this.props.markAnswerCorrect(answer);
+    }
+
+    render() {
+        var answers = [];
+        if (this.props.answers.length === 0) {
+            answers.push(
+                <p key={_.uniqueId('no_answer_')} style={styles.robotoFont}>
+                    There are no answers for this question yet. Use the answer bar to turn in a new answer.
+                </p>
+            );
+        }
+        else {
+            _.forEach(this.props.answers, (ans) => {
+                var answerToPush;
+                if (ans.isMarkedCorrect) {
+                    answerToPush = <ListItem
+                        key={_.uniqueId('answer_')}
+                        primaryText={ans.text}
+                        secondaryText="This answer is marked correct."
+                        rightIcon={<CheckIcon color={Colors.green500} />} />
+                }
+                else {
+                    answerToPush = <ListItem
+                        key={_.uniqueId('answer_')}
+                        primaryText={ans.text}
+                        onTouchTap={this.handleAnswerTap.bind(this, ans)}/>
+                }
+                answers.push(answerToPush);
+                answers.push(<Divider key={_.uniqueId('divider_')} />);
+            });
+        }
+
+        return (
+            <div>
+                <Card>
+                    <CardHeader
+                        title="Answers" />
+                    <CardText>
+                        <List>
+                            {answers}
+                        </List>
+                    </CardText>
+                </Card>
+            </div>
+        )
+    }
+}
